97d3e70ede0967b85b6f10069269feffacfa9565
Hadiyo11 <hadyhope@gmail.com>
2023-08-17 18:33:20 -0700

    new work

--- Classwork/lesson06/server_html_6.1/main.js
+++ Classwork/lesson06/server_html_6.1/main.js
@@ -3,25 +3,26 @@
 const port = 3000,
   http = require("http"),
   httpStatus = require("http-status-codes"),
-  fs = require("fs");
-const getViewUrl = url => {
+  fs = require("fs"); //require fs module , allows server to acess and read index.html
+const getViewUrl = (url) => {//set up route mapping for html files
   return `views${url}.html`;
 };
 http
   .createServer((req, res) => {
-    let viewUrl = getViewUrl(req.url);
-    fs.readFile(viewUrl, (error, data) => {
-      if (error) {
+    let viewUrl = getViewUrl(req.url);//get the file-path string
+    fs.readFile(viewUrl, (error, data) => {//read the contents of the mapped file
+      if (error) {//handles error with a 404 response code
         res.writeHead(httpStatus.NOT_FOUND);
         res.write("<h1>FILE NOT FOUND</h1>");
       } else {
         res.writeHead(httpStatus.OK, {
-          "Content-Type": "text/html"
+          "Content-Type": "text/html",
         });
-        res.write(data);
+        res.write(data);//respond with file contents
       }
       res.end();
     });
   })
   .listen(port);
-console.log(`The server has started and is listening on port number: ${port}`);
+console.log(`The server has started and is listening on port number:
+   ➥ ${port}`);
---
+++ Classwork/lesson06/server_html_6_2/main.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const port = 3000,
+  http = require("http"),
+  httpStatus = require("http-status-codes"),
+  fs = require("fs");
+
+const sendErrorResponse = res => {//created an error-handling funcrion
+  res.writeHead(httpStatus.NOT_FOUND, {
+    "Content-Type": "text/html"
+  });
+  res.write("<h1>File Not Found!</h1>");
+  res.end();
+};
+
+http
+  .createServer((req, res) => {
+    let url = req.url;//store request url in a variable
+    if (url.indexOf(".html") !== -1) {//check url to see if it contains a file extension
+      res.writeHead(httpStatus.OK, {
+        "Content-Type": "text/html"
+      });
+      customReadFile(`./views${url}`, res);
+    } else if (url.indexOf(".js") !== -1) {
+      res.writeHead(httpStatus.OK, {
+        "Content-Type": "text/javascript"
+      });
+      customReadFile(`./public/js${url}`, res);
+    } else if (url.indexOf(".css") !== -1) {
+      res.writeHead(httpStatus.OK, {
+        "Content-Type": "text/css"
+      });
+      customReadFile(`./public/css${url}`, res);
+    } else if (url.indexOf(".png") !== -1) {
+      res.writeHead(httpStatus.OK, {
+        "Content-Type": "image/png"
+      });
+      customReadFile(`./public/images${url}`, res);
+    } else {
+      sendErrorResponse(res);
+    }
+  })
+  .listen(3000);
+console.log(`The server is listening on port number: ${port}`);
+
+const customReadFile = (file_path, res) => {
+  if (fs.existsSync(file_path)) {
+    fs.readFile(file_path, (error, data) => {
+      if (error) {
+        console.log(error);
+        sendErrorResponse(res);
+        return;
+      }
+      res.write(data);
+      res.end();
+    });
+  } else {
+    sendErrorResponse(res);
+  }
+};
