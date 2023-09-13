e1258f5dbf4f6355c1ca79b6387f47e850f78fdc
Hadiyo11 <hadyhope@gmail.com>
2023-09-13 09:19:35 -0700

    new work

--- BlogProject/Chapter07/index.js
+++ BlogProject/Chapter07/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path');
 const mongoose = require('mongoose')
-
+const fileUpload = require('express-fileupload')
 const app = new express()
 const ejs = require('ejs')
 app.set('view engine', 'ejs')
@@ -9,6 +9,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded())
+app.use(fileUpload())
 
 const BlogPost = require('./models/BlogPost.js')
 
@@ -41,12 +42,18 @@ app.get('/post/:id', async (req, res) => {
     })
 })
 
-app.post('/posts/store', (req, res) => {
-    BlogPost.create(req.body)
-        .then(blogpost => res.redirect('/'))
-        .catch(error => console.log(error))
-})
+app.post('/posts/store',async (req,res) =>{    
+    let image = req.files.image
+    image.mv(path.resolve(__dirname,'public/img',image.name),
+        async (error)=>{
+            await BlogPost.create({
+                ...req.body,
+                image:'/img/' + image.name
+            })
+            res.redirect('/')
+        })
 
+})
 
 app.listen(4000, () => {
     console.log('App listening on port 4000')
