f8e326e3ffdf5736efec50e5d9bd143db23847a4
Hadiyo11 <hadyhope@gmail.com>
2023-09-24 15:22:49 -0700

    new work

--- Practice/ECommerce/app.js
+++ Practice/ECommerce/app.js
@@ -5,11 +5,18 @@ const itemRouter =require('./routers/item')
 const cartRouter = require('./routers/cart')
 const mongoose = require('mongoose')
 
-
+app.use(express.static('public'))
 app.use(express.json())
 app.use(userRouter)
 app.use(itemRouter)
 app.use(cartRouter)
+
+// const publicDirectory = path.join(__dirname, '../public')
+// app.use(express.static(publicDirectory))
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html')
+})
 
 app.listen(4000, () => {
     console.log("App listening on port 4000");
--- Practice/ECommerce/routers/user.js
+++ Practice/ECommerce/routers/user.js
@@ -1,60 +1,58 @@
-//routers handle all requests relevant to each model
+const express = require('express')
+const User = require('../models/user')
+const Auth = require('../middleware/auth')
 
-const express = require("express");
-const User = require("../models/user");
-//import user model to create new users
-const router = new express.Router();
-
-//we need a system to generate auth tokens everytime  new user
-//registers or login
+const router = new express.Router()
 
 //signup
-router.post("/users", async (req, res) => {
-  const user = new User(req.body);
-  try {
-    await user.save();
-    const token = await user.generateAuthToken();
-    res.status(201).send({ user, token });
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+
+    try {
+        await user.save()
+        const token = await user.generateAuthToken()
+        res.status(201).send({user, token})
+    } catch (error) {
+        res.status(400).send(error)
+    }
+
+})
 
 //login
-router.post("/users/login", async (req, res) => {
-  try {
-    const user = await User.findByCredentials(
-      req.body.email,
-      req.body.password
-    );
-    const token = await user.generateAuthToken();
-    res.send({ user, token });
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token})
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
 
 //logout
-router.post("/users/logout", Auth, async (req, res) => {
-  try {
-    req.user.tokens = req.user.tokens.filter((token) => {
-      return token.token !== req.token;
-    });
-    await req.user.save();
-    res.send();
-  } catch (error) {
-    res.status(500).send();
-  }
-});
+router.post('/users/logout', Auth, async (req, res) => {
+   
+    try {
+       req.user.tokens =  req.user.tokens.filter((token) => {
+            return token.token !== req.token 
+        })
 
-//clears the entire tokens array to logout out out of all devices
-router.post("/users/logoutAll", Auth, async (req, res) => {
-  try {
-    req.user.tokens = [];
-    await req.user.save();
-    res.send();
-  } catch (error) {
-    res.status(500).send();
-  }
-});
-module.exports = router;
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+//Logout All 
+router.post('/users/logoutAll', Auth, async(req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.status(500).send()        
+    }
+})
+module.exports = router
